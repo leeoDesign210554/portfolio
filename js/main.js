acb13681327639ded8bb0399ae455b7fe1f7ac13
ducnguyen122 <92997222+ducnguyen122@users.noreply.github.com>
2022-02-20 13:38:47 +0700

    fuck yess

---
+++ js/main.js
@@ -0,0 +1,12 @@
+VanillaTilt.init(document.querySelectorAll(".card-glass"), {
+    max: 10,
+    speed: 200,
+    glare: true,
+    "max-glare": .1
+});
+VanillaTilt.init(document.querySelectorAll(".card-glass-2"), {
+    max: 2,
+    speed: 200,
+    glare: true,
+    "max-glare": .2
+});
